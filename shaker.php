031e1523c3a8579776b4087dc118dcacaa6c2f99
amelnagdy <a.elnagdy87@gmail.com>
2017-10-17 01:29:01 +0200

    Implementing Freemius

--- shaker.php
+++ shaker.php
@@ -28,11 +28,73 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 Copyright 2016 WPColt.
 */
-
 include_once(plugin_dir_path(__FILE__) . "_inc/loader.php");
 $salt_shaker = new Salter();
 
 function salt_shaker_load_plugin_textdomain() {
-    load_plugin_textdomain( 'salt-shaker', FALSE, basename( dirname( __FILE__ ) ) . '/languages/' );
+	load_plugin_textdomain( 'salt-shaker', FALSE, basename( dirname( __FILE__ ) ) . '/languages/' );
 }
 add_action( 'plugins_loaded', 'salt_shaker_load_plugin_textdomain' );
+
+
+/********* Start Freemius ***********/
+// Create a helper function for easy SDK access.
+function salt_shaker_fs() {
+	global $salt_shaker_fs;
+
+	if ( ! isset( $salt_shaker_fs ) ) {
+		// Include Freemius SDK.
+		require_once dirname(__FILE__) . '/freemius/start.php';
+
+		$salt_shaker_fs = fs_dynamic_init( array(
+			'id'                  => '1476',
+			'slug'                => 'salt-shaker',
+			'type'                => 'plugin',
+			'public_key'          => 'pk_5c4b5401b7ec26fbc64a272f6db03',
+			'is_live'             => false,
+			'is_premium'          => false,
+			'has_addons'          => false,
+			'has_paid_plans'      => false,
+			'menu'                => array(
+				'slug'           => 'salt_shaker',
+				'account'        => false,
+				'contact'        => false,
+				'support'        => false,
+				'parent'         => array(
+					'slug' => 'tools.php',
+				),
+			),
+		) );
+	}
+
+	return $salt_shaker_fs;
+}
+
+// Init Freemius.
+salt_shaker_fs();
+// Signal that SDK was initiated.
+do_action( 'salt_shaker_fs_loaded' );
+
+//Welcome message after update
+function salt_shaker_fs_custom_connect_message_on_update(
+	$message,
+	$user_first_name,
+	$plugin_title,
+	$user_login,
+	$site_link,
+	$freemius_link
+) {
+	return sprintf(
+		__fs( 'hey-x' ) . '<br>' .
+		__( 'Please help us improve %2$s! If you opt-in, we\'ll collect some usage data that will help us improve the plugin. If you skip this, that\'s okay! %2$s will still work just fine.', 'salt-shaker' ),
+		$user_first_name,
+		'<b>' . $plugin_title . '</b>',
+		'<b>' . $user_login . '</b>',
+		$site_link,
+		$freemius_link
+	);
+}
+
+salt_shaker_fs()->add_filter('connect_message_on_update', 'salt_shaker_fs_custom_connect_message_on_update', 10, 6);
+
+salt_shaker_fs()->add_action('after_uninstall', 'salt_shaker_fs_uninstall_cleanup');
